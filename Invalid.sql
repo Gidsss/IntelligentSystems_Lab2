73c411b4dda9c918a57dc0dfb85873ed5ae614f1
Seajay <88035813+1202DREAMSCAPE@users.noreply.github.com>
2023-10-07 17:54:12 +0800

    Create Invalid.sql

---
+++ Invalid.sql
@@ -0,0 +1,5 @@
+insert into haha.Invalid (ID, Type, Value)
+values  (1, 'Unknown', '@hello.com'),
+        (2, 'Unknown      ', '@joelcruz.com'),
+        (3, 'Unknown      ', '123-4567'),
+        (4, 'Unknown      ', '2002-02-06');
