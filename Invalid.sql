2d51d6b0c163b749851368aedc83a1afe6573491
Gidsss <88811389+Gidsss@users.noreply.github.com>
2023-10-09 19:28:31 +0800

    Update Invalid.sql

--- Invalid.sql
+++ Invalid.sql
@@ -1,4 +1,4 @@
-insert into haha.Invalid (ID, Type, Value)
+insert into haha.invalid (ID, Type, Value)
 values  (1, 'Unknown', '@hello.com'),
         (2, 'Unknown      ', '@joelcruz.com'),
         (3, 'Unknown      ', '123-4567'),
